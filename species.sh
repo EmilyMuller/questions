91e94fe39c71ca9efa62aa0af6242cdd32f46977
EmilyMuller <emmyannemuller@gmail.com>
2014-06-17 14:52:09 -0700

    Easier to Read

--- species.sh
+++ species.sh
@@ -2,7 +2,9 @@
 #ToDo: provide links to more info on animals
 for filename in $*
 do
+	echo '////////////////////////'
 	echo $filename
+	echo '////////////////////////'
 	cut -d',' -f2 $filename | sort | uniq
 done
 
