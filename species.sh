e7a70210be3e95ecd1085db86ef12a3e36f0cf7c
EmilyMuller <emmyannemuller@gmail.com>
2014-06-17 15:16:32 -0700

    Revert "Easier to Read"
    This reverts commit 91e94fe39c71ca9efa62aa0af6242cdd32f46977.

--- species.sh
+++ species.sh
@@ -2,9 +2,7 @@
 #ToDo: provide links to more info on animals
 for filename in $*
 do
-	echo '////////////////////////'
 	echo $filename
-	echo '////////////////////////'
 	cut -d',' -f2 $filename | sort | uniq
 done
 
